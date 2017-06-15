4341ea947521239de16c1e8a786c25c041d18fa9
Patrick Edson <pedson@leapscientific.com>
2017-06-15 08:58:16 -0400

    Include main style.

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,10 +1,11 @@
+import "styles/mouselight.bootstrap.css";
+import "styles/style.css";
 import "react-select/dist/react-select.css";
 import "react-virtualized/styles.css";
 import "react-virtualized-select/styles.css";
 import "react-datepicker/dist/react-datepicker.css";
 import "react-toastify/dist/ReactToastify.min.css";
 import "rc-slider/assets/index.css";
-import "./style.css";
 export { DynamicAutoSuggestMode, IObjectAutoSuggestProps, IObjectAutoSuggestState, DynamicAutoSuggest } from "./DynamicAutoSuggest";
 export { DynamicDatePickerMode, IDynamicDatePickerProps, IDynamicDatePickerState, DynamicDatePicker } from "./DynamicDatePicker";
 export { DynamicEditFieldMode, DynamicEditField, IDynamicEditFieldProps, IDynamicEditFieldState } from "./DynamicEditField";
